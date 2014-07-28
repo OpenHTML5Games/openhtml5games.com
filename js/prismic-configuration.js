2521b40df7c0cd1196f9b2e16a75fe0943080e2e
Kai Turner <kai@kaigani.com>
2014-07-28 15:29:24 +0100

    CMS integration
    page template loads content from Prismic.io

---
+++ js/prismic-configuration.js
@@ -0,0 +1,24 @@
+var Configuration = {
+
+  // -- API endpoint
+  //apiEndpoint: 'https://lesbonneschoses.prismic.io/api',
+  apiEndpoint: 'https://openhtml5games.prismic.io/api',
+
+  // -- Access token if the Master is not open
+  // accessToken: 'xxxxxx',
+
+  // -- OAuth
+  // clientId: 'xxxxxx',
+  // clientSecret: 'xxxxxx',
+
+  // -- Links resolution rules
+  linkResolver: function(ctx, doc) {
+    return 'detail.html?id=' + doc.id + '&slug=' + doc.slug + ctx.maybeRefParam;
+  },
+
+  // -- To customize: what to do when an error happens on the prismic.io side
+  onPrismicError: function(err) {
+    window.location = '/error.html'+(err ? '#'+err.message : '');
+  }
+
+};
